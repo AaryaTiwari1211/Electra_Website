c31b85038b21e815202edf6697bef11076cf0fbb
SmitThakare <smitthakare14@gmail.com>
2024-01-25 23:00:32 +0530

    footer/nav

--- electra_main/src/components/Navbar/Navbar.jsx
+++ electra_main/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './home.css';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
-    )
+        <div className="navbar">
+            <div className="logo">
+                <span>Electra Logo</span>
+            </div>
+            <div className="nav-links">
+                <a href="#">About Us</a>
+                <a href="#">Products/Services</a>
+                <a href="#">Clients</a>
+                <a href="#">Contact Us</a>
+            </div>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
