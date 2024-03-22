b134513818cce5c8ca9425b9e696ebc0a6bda373
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2024-03-23 02:01:58 +0530

    Finished Downloads Page

--- electra_main/src/pages/AboutUs/Downloads/Downloads.jsx
+++ electra_main/src/pages/AboutUs/Downloads/Downloads.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Downloads.css'
+import { downloadsData } from '../../../utils/downloads'
+
+
 const Downloads = () => {
+
     return (
-        <>
-            <div class="downloadsviraj-child"></div>
-            <p class="thank-you-for">
-                Thank you for your interest in our company. Download our Company Profile
-                for a comprehensive overview of our skills and services.
-            </p>
-            <img class="downloadsviraj-item" alt="" src="Downloads page image.jpg" />
-        </>
+        <div className='downloads-main'>
+            <div className='downloads-left'>
+                <p class="downloads-content">
+                    {downloadsData.content}
+                </p>
+                <button className='downloads-button'>Download</button>
+            </div>
+            <img class="downloadsimage" alt="" src={downloadsData.img} />
+        </div>
     )
 }
 
---
+++ electra_main/src/utils/downloads.js
@@ -0,0 +1,7 @@
+import img1 from '../assets/Downloads.page.image/Downloads page image.jpg'
+
+export const downloadsData = {
+    content: 'We appreciate your interest in our organization. Download our Company Profile for a comprehensive overview of our skills and services.',
+    img: img1,
+    link: '#'
+}
