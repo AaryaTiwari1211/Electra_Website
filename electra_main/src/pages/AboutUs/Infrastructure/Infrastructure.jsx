d09f15b888b61507a8a63fb22634598bee49dd42
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2024-03-23 14:12:10 +0530

    Infrastructe Page Complete

--- electra_main/src/pages/AboutUs/Infrastructure/Infrastructure.jsx
+++ electra_main/src/pages/AboutUs/Infrastructure/Infrastructure.jsx
@@ -1,97 +1,37 @@
 import React from 'react'
+import { InfrastructureData } from '../../../utils/infrastructure'
 import './Infrastructure.css'
-
 
 const Infrastructure = () => {
     return (
-        <>
-            <div class="infrastructureviraj">
-                <div class="infrastructureviraj-child"></div>
-                <div class="infrastructureviraj-child"></div>
-                <p class="infrastructure">INFRASTRUCTURE</p>
-                <p class="elevating-excellence-through-container">
-                    <span class="elevating-excellence-through-container1">
-                        <span class="elevating-excellence-through-e">
-                            <b>Elevating Excellence through Empowered Expertise:</b>
-                        </span>
-                        <span class="elevating-excellence-through-e">
-                            <b>&nbsp;</b>
-                        </span>
-                        <span class="elevating-excellence-through-e">
-                            <span
-                            >At Electra Enterprises, our unparalleled strength lies in our
-                                dedicated and highly skilled workforce, where each individual is
-                                afforded equal opportunities for personal and professional
-                                development. Through a commitment to continuous training, robust
-                                career planning, fostering a positive work culture, and embracing
-                                a participative management style, we cultivate a workforce that
-                                not only meets but exceeds industry benchmarks in productivity,
-                                quality, and responsiveness.</span
-                            >
-                        </span>
-                        <span class="elevating-excellence-through-e">
-                            <span>&nbsp;</span>
-                        </span>
-                        <span class="elevating-excellence-through-e">
-                            <span
-                            >Equipped with cutting-edge machines and meticulously serviced
-                                equipment, our production unit ensures a seamless and efficient
-                                flow, setting the stage for unparalleled excellence in every
-                                endeavor.</span>
-                        </span>
-                    </span>
-                </p>
-
-                <div class="infrastructureviraj-inner"></div>
-                <p class="making-flawless-products-container">
-                    <span class="elevating-excellence-through-e"
-                    >MAKING FLAWLESS PRODUCTS WITH TOP-NOTCH MACHINES</span
-                    >
-                </p>
-                <div class="rectangle-div">
-                    <img class="amada-punching" alt="" src="./AMADA PUNCHING MACHINE.jpg" />
-                </div>
-
-                <div class="infrastructureviraj-child1"></div>
-
-                <div class="infrastructureviraj-child2"></div>
-                <div class="infrastructureviraj-child3"></div>
-                <div class="infrastructureviraj-child4"></div>
-
-                <img class="amada-bending-machine-1" alt="" src="./AMADA BENDING MACHINE.jpg" />
-
-                <img class="image-4-icon" alt="" src="./Powder coating plant.jpg" />
-
-                <img class="image-5-icon" alt="" src="./7 tank.jpg" />
-
-                <p class="amada-ae-2510-container">
-                    <span class="elevating-excellence-through-e">AMADA</span>
-                    <span class="elevating-excellence-through-e"
-                    >AE 2510 NT Punching M/c</span
-                    >
-                </p>
-                <p class="amada-rg-m2-container">
-                    <span class="elevating-excellence-through-e">AMADA </span>
-                    <span class="elevating-excellence-through-e"
-                    >RG M2 1003 Bending M/c</span
-                    >
-                </p>
-                <p class="conveyorised-powder-coating">
-                    Conveyorised Powder Coating Plant
-                </p>
-                <p class="busbar-machine-container">
-                    <span class="elevating-excellence-through-e">Busbar Machine </span>
-                </p>
-                <p class="tank-pretreatment-process-container">
-                    <span class="elevating-excellence-through-e"
-                    >7 tank Pretreatment Process</span
-                    >
-                </p>
-                <img class="image-6-icon" alt="" src="./BUSBAR MACHINE.jpg" />
-
-                <p class="and-more">And more....</p>
+        <div className='infrastructure-main'>
+            <div className="section-1">
+                <p>INFRASTRUCTURE</p>
             </div>
-        </>
+            <div className="section-2">
+                <h2>{InfrastructureData.about.head}</h2>
+                <p>{InfrastructureData.about.content}</p>
+            </div>
+            <div className="section-3">
+                <p className='section-3-heading'>{InfrastructureData.items_sections.head}</p>
+                <div className="section-3-items">
+                    {InfrastructureData.items_sections.items.map((item, index) => {
+                        const isOdd = index % 2 === 0; 
+                        const flexStyle = isOdd ? { flexDirection: 'row' } : { flexDirection: 'row-reverse' };
+
+                        return (
+                            <div className="section-3-item" key={index} style={flexStyle}>
+                                <img className='section-3-item-image' src={item.img} alt="item" />
+                                <div className='section-3-item-content'>
+                                    <p className='section-3-item-brand'>{item.brand}</p>
+                                    <p className='section-3-item-model'>{item.model}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ electra_main/src/utils/infrastructure.js
@@ -0,0 +1,41 @@
+
+import img1 from '../assets/Infrastructure.page.images/AMADA PUNCHING MACHINE.jpg';
+import img2 from '../assets/Infrastructure.page.images/AMADA BENDING MACHINE.jpg';
+import img3 from '../assets/Infrastructure.page.images/Powder coating plant.jpg';
+import img4 from '../assets/Infrastructure.page.images/7 tank.jpg';
+import img5 from '../assets/Infrastructure.page.images/BUSBAR MACHINE.jpg';
+
+
+export const InfrastructureData = {
+    about: {
+        head: 'Elevating Excellence through Empowered Expertise:',
+        content: 'At Electra Enterprises, our unparalleled strength lies in our dedicated and highly skilled workforce, where each individual is afforded equal opportunities for personal and professional development. Through a commitment to continuous training, robust career planning, fostering a positive work culture, and embracing a participative management style, we cultivate a workforce that not only meets but exceeds industry benchmarks in productivity, quality, and responsiveness. Equipped with cutting-edge machines and meticulously serviced equipment, our production unit ensures a seamless and efficient flow, setting the stage for unparalleled excellence in every endeavor.'
+    },
+    items_sections: {
+        head: 'MAKING FLAWLESS PRODUCTS WITH TOP-NOTCH MACHINES',
+        items: [
+            {
+                brand: 'AMADA',
+                model: 'AE 2510 NT Punching M/c',
+                img: img1
+            },
+            {
+                brand: 'AMADA',
+                model: 'RG M2 1003 Bending M/c',
+                img: img2
+            },
+            {
+                model: 'Conveyorised Powder Coating Plant',
+                img: img3
+            },
+            {
+                model: '7 tank Pretreatment Process',
+                img: img4
+            },
+            {
+                model: 'Busbar Machine',
+                img: img5
+            },
+        ]
+    }
+}
