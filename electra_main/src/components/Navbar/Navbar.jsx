328f8142086790a2bd4727c411759f6410a219d3
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2024-03-25 19:38:22 +0530

    Added Drawer Component

--- electra_main/src/components/Navbar/Navbar.jsx
+++ electra_main/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { navbarData } from '../../utils/navbar';
 import { useNavigate } from 'react-router-dom';
+import menu from '../../assets/menu.svg';
+import logo from '../../assets/logo1.jpg';
+import { useEffect } from 'react';
 
+
+const useClickOutside = (ref, callback) => {
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                callback();
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [ref, callback]);
+}
 const Navbar = () => {
     const { logo, links } = navbarData;
     const Navigate = useNavigate();
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+    };
+
+    const drawerRef = React.useRef(null);
+    useClickOutside(drawerRef, () => setDrawerOpen(false));
+
     return (
         <div className="navbar">
-            <div className="logo" onClick={()=>Navigate('/')}>
-                <img src={logo} alt="Electra Logo" style={{
-                    cursor: 'pointer'
-                }} />
+            <div className="logo" onClick={() => Navigate('/')}>
+                <img src={logo} alt="Logo" />
             </div>
+
             <div className="nav-links">
-                {
-                    links.map((link, index) => {
-                        return (
-                            <a href={link.url} key={index} className=''>{link.title}</a>
-                        )
-                    })
-                }
+                {links.map((link, index) => (
+                    <a href={link.url} key={index}>{link.title}</a>
+                ))}
+            </div>
+
+            <button className="menu-icon" onClick={toggleDrawer}>
+                <img src={menu} alt="" />
+            </button>
+
+            <div ref={drawerRef} className={`drawer ${drawerOpen ? 'open' : ''}`}>
+                {links.map((link, index) => (
+                    <a href={link.url} key={index} onClick={toggleDrawer}>{link.title}</a>
+                ))}
+                <a href="/aboutus/infrastructure" onClick={toggleDrawer}>Infrastructure</a>
+                <a href="/aboutus/qualitypolicy" onClick={toggleDrawer}>Quality Policy</a>
             </div>
         </div>
     );
